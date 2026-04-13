ccd75087193dec2a3ff1034166fac3e9dcd23b1e
mitch <mitchdamayo16@gmail.com>
2026-04-13 14:42:43 +0800

    driver update

--- controller/driver/DriverUpdateController.php
+++ controller/driver/DriverUpdateController.php
@@ -146,7 +146,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             // Determine redirect path
             if (isset($_POST['dashboard_view']) || isset($_GET['dashboard_view'])) {
-                header("Location: index.php?page=settings&success=1");
+                header("Location: ../../view/driver/settings.php?page=settings&success=1");
             } else {
                 header("Location: ../../view/driver/settings.php?success=1");
             }
